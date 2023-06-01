2a0b6ebb3a75ba94d0c36a1b36c6a69fe6c3af46
Anton1919 <bf3kenzo@mail.ru>
2023-06-01 22:01:42 +0300

    refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,18 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import './styles/index.scss'
-import {Link, Route, Routes} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
+import {NavBar} from "widgets/NavBar";
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme()
+    const {theme} = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>Change theme</button>
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/'} element={<MainPage/>}/>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                </Routes>
-            </Suspense>
+            <NavBar/>
+            <AppRouter/>
+
         </div>
     );
 };
